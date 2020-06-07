d81b7d67f5ef6eac3a7195c2bd7f42d87d55e5b4
李金磊 <leiaiyaliu@gmail.com>
2020-06-07 23:42:32 +0800

    部署2

---
+++ src/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  base: '/blog',
+  dest: './docs',
+  title: 'Lay',
+  description: 'lay的博客',
+  themeConfig: {
+    sidebar: 'auto'
+  }
+}
---
+++ src/README.md
@@ -0,0 +1,6 @@
+---
+home: true
+sidebar: auto
+---
+# [图片懒加载](./lazy-img.md)
+# [自适应字体](./adaptive-fontsize.md)
