4ecc59ad2b87596674d9c65d98ac3e8a3a920719
sunyaping950306 <51746004+sunyaping950306@users.noreply.github.com>
2020-06-09 23:40:17 +0800

    文章按照修改时间排序

--- src/.vuepress/utils/index.js
+++ src/.vuepress/utils/index.js
@@ -1,28 +1,56 @@
 const path = require('path')
 const fs = require('fs')
+genSliderBar()
 function genSliderBar() {
   const basePath = path.resolve(__dirname, '..', '..', 'zh')
-  const result = []
   const _readdir = (path, arr, depth) => {
     const dirsOrFiles = fs.readdirSync(basePath + path)
     dirsOrFiles.forEach(name => {
       const stat = fs.statSync(basePath + path + name)
       if (stat.isDirectory()) {
+        const rawChildren = _readdir(path + name + '/', [], depth + 1)
+        const sortChildren = mergeSort(rawChildren, (a, b) => a.mtime < b.mtime)
         const group = {
           title: name,
           sidebarDepth: depth,
-          children: []
+          children: sortChildren.map(item => item.path)
         }
         arr.push(group)
-        _readdir(path + name + '/', group.children, depth + 1)
       } else {
-        arr.push('/zh' + path + name)
+        arr.push({
+          path: '/zh' + path + name,
+          mtime: stat.mtime
+        })
       }
     })
+    return arr
   }
-  _readdir('/', result, 1)
-  return result
+  return _readdir('/', [], 1)
 }
+function mergeSort (array, fn) {
+  if (!fn || typeof fn !== 'function') fn = (a, b) => a > b
+  // 分
+  const divide = (arr) => {
+    const len = arr.length
+    if (len < 2) return arr
+    const mid = len / 2 | 0
+    return merge(divide(arr.slice(0, mid)), divide(arr.slice(mid)))
+  }
+  // 合
+  const merge = (a1, a2) => {
+    const a = []
+    while (a1.length && a2.length) {
+      a.push(fn(a1[0], a2[0]) 
+        ? a2.shift()
+        : a1.shift()
+        )
+    }
+    return a.concat(a1, a2)
+  }
+  return divide(array)
+}
+
+
 module.exports = {
   genSliderBar
 }
