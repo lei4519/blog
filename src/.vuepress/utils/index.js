e49d170d53aa54b3c0488617c383761ec12a2fda
李金磊 <leiaiyaliu@gmail.com>
2020-06-09 18:14:20 +0800

    国际化配置

--- src/.vuepress/utils/index.js
+++ src/.vuepress/utils/index.js
@@ -1,14 +1,11 @@
 const path = require('path')
 const fs = require('fs')
 function genSliderBar() {
-  const noParse = ['README.md']
-  const basePath = path.resolve(__dirname, '..', '..')
+  const basePath = path.resolve(__dirname, '..', '..', 'zh')
   const result = []
   const _readdir = (path, arr, depth) => {
     const dirsOrFiles = fs.readdirSync(basePath + path)
     dirsOrFiles.forEach(name => {
-      if (name.indexOf('.') === 0) return
-      if (noParse.includes(name)) return
       const stat = fs.statSync(basePath + path + name)
       if (stat.isDirectory()) {
         const group = {
@@ -19,14 +16,13 @@ function genSliderBar() {
         arr.push(group)
         _readdir(path + name + '/', group.children, depth + 1)
       } else {
-        arr.push(path + name)
+        arr.push('/zh' + path + name)
       }
     })
   }
   _readdir('/', result, 1)
   return result
 }
-genSliderBar()
 module.exports = {
   genSliderBar
 }
--- src/README.md
+++ src/README.md
@@ -1,9 +1,10 @@
 ---
 home: true
+lang: zh-cn
 heroText: Hero 标题
 tagline: Hero 副标题
 actionText: 快速上手 →
-actionLink: /blog/vue-press
+actionLink: /zh/blog/vue-press
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
