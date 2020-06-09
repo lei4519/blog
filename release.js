1c7f5deaa3363a51186e85014fef7fb8ba82d05a
李金磊 <leiaiyaliu@gmail.com>
2020-06-09 16:47:14 +0800

    测试脚本

---
+++ release.js
@@ -0,0 +1,38 @@
+let commitMsg = process.argv[2];
+const stdin = process.stdin;
+
+if (!commitMsg) {
+  console.log("请输入git提交信息：");
+} else {
+  sh()
+}
+function sh() {
+  const spawn = require("child_process").spawn;
+  const subProcess = spawn("bash");
+  function onData(data) {
+    process.stdout.write(data);
+  }
+  subProcess.on("error", function() {
+    console.log("error");
+    console.log(arguments);
+  });
+  subProcess.stdout.on("data", onData);
+  subProcess.stderr.on("data", onData);
+  subProcess.on("close", (code) => {
+    console.log(`打包完成：${code}`);
+  });
+  subProcess.stdin.write(`yarn build
+  git add .
+  git commit -m ${commitMsg}
+  git push`);
+  subProcess.stdin.end();
+}
+stdin.on("data", (data) => {
+  data = data.toString();
+  if (!data.toString().trim()) {
+    console.log("请输入git提交信息：");
+  } else {
+    commitMsg = data;
+    sh()
+  }
+});
--- src/README.md
+++
@@ -1,6 +0,0 @@
----
-home: true
-sidebar: auto
----
-# [图片懒加载]($withBase('/lazy-img.md'))
-# [自适应字体]($withBase('/adaptive-fontsize.md'))
