ee6c194cb33b5c8260de0bd5d400f48dc619c070
Lay <939690017@qq.com>
2021-03-20 22:41:43 +0800

    小程序框架原理总结

--- release.js
+++ release.js
@@ -1,36 +1,24 @@
+const shell = require('shelljs')
 function sh(commitMsg) {
-  const spawn = require("child_process").spawn;
-  const subProcess = spawn("bash");
-  function onData(data) {
-    process.stdout.write(data);
-  }
-  subProcess.on("error", function() {
-    console.log("error");
-    console.log(arguments);
-  });
-  subProcess.stdout.on("data", onData);
-  subProcess.stderr.on("data", onData);
-  subProcess.on("close", (code) => {
-    console.log(`打包完成：${code}`);
-    process.exit(code)
-  });
-  subProcess.stdin.write(`vuepress build src
-  git add .
-  git commit -m "${commitMsg}"
-  git push`);
-  subProcess.stdin.end();
+  shell.exec('vuepress build src')
+  shell.exec('git add .')
+  shell.exec(`git commit -m "${commitMsg}"`)
+  shell.exec('git push')
+
+  shell.echo('打包完成')
+  shell.exit(0)
 }
 
 if (!process.argv[2]) {
-  process.stdin.on("data", (data) => {
-    data = data.toString();
+  process.stdin.on('data', data => {
+    data = data.toString()
     if (!data.toString().trim()) {
-      console.log("请输入git提交信息：");
+      console.log('请输入git提交信息：')
     } else {
       sh(data)
     }
-  });
-  console.log("请输入git提交信息：");
+  })
+  console.log('请输入git提交信息：')
 } else {
   sh(process.argv[2])
 }
--- src/notes/Web/get.md
+++ src/notes/Web/get.md
@@ -1,5 +1,6 @@
 # 安全的对象属性获取 - get
 
+```js
 const get = (obj, propStr, defaultValue) => {
   try {
     const props = propStr.split('.')
@@ -12,3 +13,4 @@ const get = (obj, propStr, defaultValue) => {
     return defaultValue
   }
 }
+```
